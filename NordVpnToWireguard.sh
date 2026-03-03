eb8626fec7de08ef8e4fdd893a0a5615161efe63
Andrew Suryanto <121226201+p1e2r@users.noreply.github.com>
2026-03-03 19:03:52 +0700

    Upgrade NordVpnToWireguard script to version 1.0.0
    Updated script to version 1.0.0 and improved option handling.

--- NordVpnToWireguard.sh
+++ NordVpnToWireguard.sh
@@ -1,126 +1,109 @@
-#!\/bin\/bash
-# shellcheck disable=SC2155
-#
-# NordVpnToWireguard
-#
-# Server Files: \/mnt\/server
+#!/bin/bash
 
 COUNTRY=""
 CITY=""
 SERVER=""
-VERSION="0.1.0"
+VERSION="1.0.0"
+
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as sudo."
+  echo "Run: sudo $0 [options]"
+  exit 1
+fi
+
+REQUIRED_PKGS=()
+
+command -v wg >/dev/null 2>&1 || REQUIRED_PKGS+=("wireguard")
+command -v curl >/dev/null 2>&1 || REQUIRED_PKGS+=("curl")
+command -v jq >/dev/null 2>&1 || REQUIRED_PKGS+=("jq")
+command -v ifconfig >/dev/null 2>&1 || REQUIRED_PKGS+=("net-tools")
+command -v getent >/dev/null 2>&1 || REQUIRED_PKGS+=("libc-bin")
+
+if [ ${#REQUIRED_PKGS[@]} -ne 0 ]; then
+  apt update
+  apt install -y "${REQUIRED_PKGS[@]}"
+fi
+
+nordvpn set technology nordlynx || exit 1
 
 while [ "$1" != "" ]; do
   case $1 in
-  -v | --version)
-    echo "Wireguard Config Files for NordVPN v$VERSION"
-    exit
-    ;;
-  -c | --country)
-    shift
-    if [ -n "$1" ]; then
-      COUNTRY="$1"
-    fi
-    ;;
-  -s | --city)
-    shift
-    if [ -n "$1" ]; then
-      CITY="$1"
-    fi
-    ;;
-  -server)
-    shift
-    if [ -n "$1" ]; then
-      SERVER="$1"
-    fi
-    ;;
-  -h | --help)
-    echo "Usage: NordVpnToWireguard [OPTIONS]"
-    echo "OPTION includes:"
-    echo "   -v | --version  - prints out version information."
-    echo "   -c | --country  - Country to connect to (ex. Canada)."
-    echo "   -s | --city     - City to connect to (ex. Toronto)."
-    echo "   -server         - Server to connect to (ex. us8577)."
-    echo "   -h | --help     - displays this message."
-    exit
-    ;;
-  *)
-    echo "Invalid option: $1"
-    echo "Usage: NordVpnToWireguard [-v] [-c country] [-s server] [-server specific-server]"
-    echo "   -v | --version   - prints out version information."
-    echo "   -c | --country   - country name"
-    echo "   -s | --city      - city name"
-    echo "   -server         - specific server name"
-    echo "   -h | --help      - displays this message."
-    exit
-    ;;
+    -v | --version)
+      echo "NordVPN → WireGuard (Gluetun) v$VERSION"
+      exit
+      ;;
+    -c | --country)
+      shift
+      [ -n "$1" ] && COUNTRY="$1"
+      ;;
+    -s | --city)
+      shift
+      [ -n "$1" ] && CITY="$1"
+      ;;
+    -server)
+      shift
+      [ -n "$1" ] && SERVER="$1"
+      ;;
+    -h | --help)
+      echo "Usage: sudo $0 [OPTIONS]"
+      echo "  -c | --country  Country name"
+      echo "  -s | --city     City name"
+      echo "  -server         Specific server"
+      exit
+      ;;
+    *)
+      exit 1
+      ;;
   esac
   shift
 done
 
-echo "Preparing to connect..."
-
 if [[ -n "$SERVER" ]]; then
-  echo "Connecting to server $SERVER..."
-  nordvpn c $SERVER
-elif [[ -z "$COUNTRY" ]] && [[ -z "$CITY" ]]; then
-  echo "Getting configuration for recommended server..."
+  nordvpn c "$SERVER"
+elif [[ -z "$COUNTRY" && -z "$CITY" ]]; then
   nordvpn c
+elif [[ -n "$COUNTRY" && -z "$CITY" ]]; then
+  nordvpn c "$COUNTRY"
+elif [[ -z "$COUNTRY" && -n "$CITY" ]]; then
+  nordvpn c "$CITY"
 else
-  if [[ -z "$CITY" ]] && [[ ! -z "$COUNTRY" ]]; then
-    echo "Getting configuration for recommended server in $COUNTRY..."
-    nordvpn c $COUNTRY
-  fi
-
-  if [[ ! -z "$CITY" ]] && [[ -z "$COUNTRY" ]]; then
-    echo "Getting configuration for recommended server in $CITY..."
-    nordvpn c $CITY
-  fi
-
-  if [[ ! -z "$CITY" ]] && [[ ! -z "$COUNTRY" ]]; then
-    echo "Getting configuration for recommended server in $COUNTRY, city: $CITY..."
-    nordvpn c $COUNTRY $CITY
-  fi
+  nordvpn c "$COUNTRY" "$CITY"
 fi
 
 if [ $? -ne 0 ]; then
-  echo "Unable to connect to NordVPN."
   exit 1
 fi
 
-# Preparing the Interface section
-echo "[Interface]" >Nordvpn.conf
-privateKey=$(sudo wg show nordlynx private-key)
-echo "PrivateKey = $privateKey" >>Nordvpn.conf
-echo "ListenPort = 51820" >>Nordvpn.conf
-localAddress=$(ifconfig nordlynx | grep inet | awk -v OFS='\n' '{ print $2 }')
-echo "Address = $localAddress/32" >>Nordvpn.conf
-echo "DNS = 103.86.96.100, 103.86.99.100" >>Nordvpn.conf
-echo "" >>Nordvpn.conf
+echo "[Interface]" > wg0.conf
 
-# Gathering info for the Peer section
-curl -s "https://api.nordvpn.com/v1/servers/recommendations?&filters\[servers_technologies\]\[identifier\]=wireguard_udp&limit=1" | jq -r '.[]|.hostname, .station, (.locations|.[]|.country|.city.name), (.locations|.[]|.country|.name), (.technologies|.[].metadata|.[].value), .load' >>Peer.txt
+privateKey=$(wg show nordlynx private-key)
+echo "PrivateKey = $privateKey" >> wg0.conf
 
-# Disconnect from NordVPN
+localAddress=$(ifconfig nordlynx | grep inet | awk '{ print $2 }')
+echo "Address = $localAddress/32" >> wg0.conf
+
+echo "DNS = 103.86.96.100, 103.86.99.100" >> wg0.conf
+echo "" >> wg0.conf
+
+curl -s "https://api.nordvpn.com/v1/servers/recommendations?&filters[servers_technologies][identifier]=wireguard_udp&limit=1" \
+| jq -r '.[]|.hostname, (.technologies|.[].metadata|.[].value)' > peer.tmp
+
+endpointHost=$(head -n 1 peer.tmp)
+publicKey=$(tail -n 1 peer.tmp)
+rm peer.tmp
+
 nordvpn d >/dev/null 2>&1
 
-# Preparing the Peer section
-endpoint=$(grep -m 1 -o '.*' Peer.txt | tail -n 1)
-publicKey=$(grep -m 5 -o '.*' Peer.txt | tail -n 1)
+endpointIP=$(getent ahostsv4 "$endpointHost" | awk '{ print $1 }' | head -n 1)
 
-rm Peer.txt
+if [ -z "$endpointIP" ]; then
+  exit 1
+fi
 
-echo "[Peer]" >>Nordvpn.conf
-echo "PublicKey = $publicKey" >>Nordvpn.conf
-echo "AllowedIPs = 0.0.0.0/0" >>Nordvpn.conf
-echo "Endpoint = $endpoint:51820" >>Nordvpn.conf
-echo "PersistentKeepalive = 25" >>Nordvpn.conf
+echo "[Peer]" >> wg0.conf
+echo "PublicKey = $publicKey" >> wg0.conf
+echo "AllowedIPs = 0.0.0.0/0" >> wg0.conf
+echo "Endpoint = $endpointIP:51820" >> wg0.conf
+echo "PersistentKeepalive = 25" >> wg0.conf
 
-# Renaming config file to show the endpoint country id and server number
-outputFileName=$(echo $endpoint | grep -o '^[^.]*')
-outputFileName=$(echo "NordVPN-$outputFileName.conf")
-
-mv Nordvpn.conf $outputFileName
-
-echo "Wireguard configuration file $outputFileName created successfully!"
 exit 0
